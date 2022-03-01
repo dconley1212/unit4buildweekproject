ca1c2357f2d0d395895b47254a84407eae22b40f
David Conley <dconley1989@gmail.com>
2022-03-01 09:18:26 -0700

    added middleware for user registration and login

--- api/server.js
+++ api/server.js
@@ -1,28 +1,21 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const db = require('./data/db-config')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const usersRouter = require("./users/users-router");
 
-function getAllUsers() { return db('users') }
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+server.use("/api/users", usersRouter);
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    custom: "something went wrong",
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
-
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
-
-module.exports = server
+module.exports = server;
---
+++ api/users/users-middleware.js
@@ -0,0 +1,48 @@
+const { getUserByFilter } = require("./users-model");
+
+const checkAllRequiredFields = (req, res, next) => {
+  const { username, password, phone_number } = req.body;
+  if (!username) {
+    next({ status: 400, message: "missing username" });
+  } else if (!password) {
+    next({ status: 400, message: "missing password" });
+  } else if (!phone_number) {
+    next({ status: 400, message: "missing phone number" });
+  } else {
+    next();
+  }
+};
+
+const usernameUnique = async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const usernameExists = await getUserByFilter({ username });
+    if (usernameExists) {
+      next({ status: 400, message: "username already exists" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const phoneNumberUnique = async (req, res, next) => {
+  try {
+    const { phone_number } = req.body;
+    const phoneNumberExists = await getUserByFilter({ phone_number });
+    if (phoneNumberExists) {
+      next({ status: 400, message: "phone number already exists" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  checkAllRequiredFields,
+  usernameUnique,
+  phoneNumberUnique,
+};
