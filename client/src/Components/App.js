7c48b30efa498fc391e5627244cc7bab184be884
David Conley <dconley1989@gmail.com>
2022-03-14 11:20:01 -0600

    added styling to landing and login page

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,29 +1,88 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import Register from "./Register";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: row-reverse;
+  padding: 10px;
+`;
+
+const StyledLinks = styled.div`
+  display: block;
+  padding: 5px;
+`;
+
+const StyledNavLink = styled(Link)`
+  padding: 10px;
+  margin: 10px;
+  color: #f5fffa;
+  font-size: 1.3rem;
+`;
+
+const StyledTitle = styled.h1`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  color: #000000;
+`;
+
+const StyledSlogan = styled.h2`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.7rem;
+  color: #000000;
+  margin: 0px;
+`;
+
+const StyledActionParagraph = styled.p`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  color: #000000;
+  margin: 0px;
+`;
 
 function App() {
   return (
     <div>
-      <header>
-        <Link to="/login">Login</Link>
-        <Link to="/login">Logout</Link>
-        <Link to="/register">Create an Account</Link>
-      </header>
-      <Route path="/dashboard">
-        <Dashboard />
-      </Route>
-      <Route path="/register">
-        <Register />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route exact path="/">
-        <Login />
-      </Route>
+      <StyledHeader>
+        <header>
+          <StyledLinks>
+            <StyledNavLink to="/login">Login</StyledNavLink>
+            <StyledNavLink to="/login">Logout</StyledNavLink>
+            <StyledNavLink to="/register">Register</StyledNavLink>
+          </StyledLinks>
+        </header>
+      </StyledHeader>
+      <StyledTitle>Healthy Plants</StyledTitle>
+      <StyledSlogan>Anyone can learn how to have a green thumb!</StyledSlogan>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Login />
+          <StyledActionParagraph>
+            Register or Login for tips.
+          </StyledActionParagraph>
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,6 +1,68 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
+import styled, { createGlobalStyle, css } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html {
+    height: 100%
+}
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    background: linear-gradient(to bottom, #8FBC8F, #3CB371);
+    height: 100%;
+    margin: 0;
+    color: #555;
+}
+`;
+
+const sharedStyles = css`
+  background-color: #eee;
+  height: 40px;
+  border-radius: 5px;
+  border: 1px;
+  margin: 10px 0 20px 0;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+const StyledFormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+  padding: 0 10px;
+`;
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 500px;
+  padding: 40px;
+  background-color: #fff;
+  border-radius: 10px
+  box-sizing: border-box;
+  box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.2);
+  `;
+
+const StyledInput = styled.input`
+  display: block;
+  width: 100%;
+  ${sharedStyles}
+`;
+
+const StyledButton = styled.button`
+  display: block;
+  background-color: #8fbc8f;
+  color: #fff;
+  font-size: 0.9rem;
+  border: 0;
+  border-radius: 5px;
+  height: 40px;
+  padding: 0 20px;
+  cursor: pointer;
+  box-sizing: border-box;
+`;
 
 const Login = () => {
   const [form, setForm] = useState({
@@ -32,27 +94,30 @@ const Login = () => {
 
   return (
     <div>
-      <form onSubmit={handleLoginSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={form.username}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={form.password}
-            onChange={handleChange}
-          ></input>
-        </label>
-        <button>Login</button>
-      </form>
+      <GlobalStyle />
+      <StyledFormWrapper>
+        <StyledForm onSubmit={handleLoginSubmit}>
+          <label>
+            Username:
+            <StyledInput
+              type="text"
+              name="username"
+              value={form.username}
+              onChange={handleChange}
+            ></StyledInput>
+          </label>
+          <label>
+            Password:
+            <StyledInput
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+            ></StyledInput>
+          </label>
+          <StyledButton>Login</StyledButton>
+        </StyledForm>
+      </StyledFormWrapper>
     </div>
   );
 };
