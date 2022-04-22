f655da85ec0d7cca0f47db6ec5b233dad1a5e59f
David Conley <dconley1989@gmail.com>
2022-04-22 10:47:12 -0600

    created a private route component to secure the routes of each page and need to add them to the routes on app.js

---
+++ client/src/Components/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+function PrivateRoute(props) {
+  const { children, ...rest } = props;
+
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (localStorage.getItem("token")) {
+          return children;
+        } else {
+          return <Redirect to="/" />;
+        }
+      }}
+    />
+  );
+}
+
+export default PrivateRoute;
