529e6124e368f03ca50c0bb4b2cbc55b9a824292
David Conley <dconley1989@gmail.com>
2022-05-19 16:33:32 -0600

    reverted back to the old base url because that seemed to work better now that I have seen the changes

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -7,7 +7,7 @@ const axiosWithAuth = () => {
     headers: {
       authorization: token,
     },
-    baseURL: "https://waterplantsapp.herokuapp.com/",
+    baseURL: "http://localhost:9000/api",
   });
 };
 
