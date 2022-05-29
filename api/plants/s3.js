4e053cb2dab3ffd06a58d4b39ae0ee2bbfe16cd6
David Conley <dconley1989@gmail.com>
2022-05-29 12:20:21 -0600

    added functionality to the s3 component and I need to encrypt the image name to complete it

--- api/plants/s3.js
+++ api/plants/s3.js
@@ -1,9 +1,11 @@
 const aws = require("aws-sdk");
 
+//I left off needing to download the crypto package to encrypt the imageName
+
 const region = "us-west-2";
 const bucketName = "water-plants-app-plant-images";
-const accessKeyId = "";
-const secretAccessKey = "";
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3 = new aws.S3({
   region,
@@ -11,3 +13,17 @@ const s3 = new aws.S3({
   secretAccessKey,
   signatureVersion: "4",
 });
+
+async function generateUploadUrl() {
+  const imageName = "random image name";
+
+  const params = {
+    Bucket: bucketName,
+    Key: imageName,
+    Expires: 60,
+  };
+  const uploadURL = await s3.getSignedUrlPromise("putObject", params);
+  return uploadURL;
+}
+
+module.exports = { generateUploadUrl };
