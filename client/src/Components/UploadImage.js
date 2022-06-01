46262b0c8c4830bbe9b6abb115f6bc543329a894
David Conley <dconley1989@gmail.com>
2022-06-01 15:52:55 -0600

    created the structure for how the images will be uploaded and viewed. Additionally I created helper function to update the s3url to upload the file to the s3 bucket but can get it to render on the screen yet

--- client/src/Components/UploadImage.js
+++ client/src/Components/UploadImage.js
@@ -1,23 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const UploadImage = (props) => {
-    const [ urlString ] = props
-    const [file, setFile] = useState('')
+  const [file, setFile] = useState(null);
+  const [image, setImage] = useState(null);
 
-    const handleFile = () => {
-        
-    }
+  const handleFile = (e) => {
+    setFile(e.target.files[0]);
+    axios.put(props.urlString, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      body: file,
+    });
+  };
 
-    const handleUpload = () => {
-
-    }
+  const handleUpload = () => {
+    const imageUrl = props.urlString.split("?")[0];
+    setImage(imageUrl);
+  };
 
   return (
     <>
-      {image === "" ? <form>
-        <input type="file" ></input>
-        <button>Upload</button>
-      </form>}
+      {image === null ? (
+        <form onSubmit={handleUpload}>
+          <input type="file" onChange={handleFile}></input>
+          <button>Upload</button>
+        </form>
+      ) : (
+        <img src={image} alt="plants"></img>
+      )}
     </>
   );
 };
