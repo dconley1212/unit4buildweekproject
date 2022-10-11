5c1d30c63e25dc6c0c572de32fb17a4ed50cfb97
David Conley <dconley1989@gmail.com>
2022-10-10 18:02:45 -0600

    added a couple more tests to the plants-model file to test the getAllPlants function for the plants table in the database

--- api/__tests__/plants-model.test.js
+++ api/__tests__/plants-model.test.js
@@ -1,8 +1,28 @@
 const plantsModel = require("../plants/plants-model");
+const { getAllPlants } = require("../plants/plants-model");
 const db = require("../data/db-config");
 
 test("it is the correct environment for the tests", () => {
   expect(process.env.TESTING_DATABASE_URL).toBe(
     "postgresql://postgres:AmyandDave101918@localhost:5432/waterplants_test"
   );
+});
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db.seed.run();
+});
+
+describe("plants db access functions", () => {
+  describe("plantModel.getAll", () => {
+    test("getAll plants function gives you all plants", async () => {
+      const plants = await getAllPlants();
+      expect(plants.length).toBe(3);
+    });
+    test("resolve to the correct plant shape", () => {});
+  });
 });
