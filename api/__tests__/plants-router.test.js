ef0b9fb31cfaa51d5265fb407b66af2d7bfcd6a0
David Conley <dconley1989@gmail.com>
2022-10-11 18:36:03 -0600

    added another test to see if I would get a 200 status for the get request for users plants and am getting a jest error that I need to figure out next

--- api/__tests__/plants-router.test.js
+++ api/__tests__/plants-router.test.js
@@ -2,14 +2,27 @@ const plantsRouter = require("../plants/plants-router");
 const db = require("../data/db-config");
 const request = require("supertest");
 
-beforeAll(async () => {
+let token;
+
+beforeAll(async (done) => {
   await db.migrate.rollback();
   await db.migrate.latest();
+  await db.seed.run();
+  await request("http://localhost:9000/api/users/auth")
+    .post("/login")
+    .send({
+      username: "amyconley",
+      password: "lkavgs",
+      phone_number: 8014344556,
+    })
+    .end((err, res) => {
+      token = res.body.token;
+    });
 });
 
-beforeEach(async () => {
-  await db.seed.run();
-});
+// beforeEach(async (done) => {
+
+// });
 
 afterAll(async () => {
   await db.destroy();
@@ -22,5 +35,12 @@ describe("All APIs for the plant router", () => {
       `/${user_id}`
     );
     expect(res.status).toBe(404);
+  });
+  test("[GET] /:user_id return 200 ok status", async () => {
+    const user_id = 1;
+    const res = await request("http://localhost:9000/api/plants")
+      .get(`/${user_id}`)
+      .send(token);
+    expect(res.status).toBe(200);
   });
 });
