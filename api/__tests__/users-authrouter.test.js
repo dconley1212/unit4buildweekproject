4147d10ed36a6a6e5aa95b0e4885f6429df0dd25
David Conley <dconley1989@gmail.com>
2022-10-20 09:28:27 -0600

    added another test to make sure the middleware was working for the register api for when a user already exists

--- api/__tests__/users-authrouter.test.js
+++ api/__tests__/users-authrouter.test.js
@@ -18,14 +18,24 @@ afterAll(async () => {
 describe("all APIs for authentication and authorization", () => {
   test("[POST] /register api is giving a 200 ok status", async () => {
     const newUser = {
-      username: "dconley",
-      password: "Walter123*",
-      phone_number: "8017778565",
+      username: "aConley",
+      password: "Romee123*",
+      phone_number: "8017775785",
     };
     const res = await request("http://localhost:9000/api/users/auth")
       .post("/register")
       .send(newUser);
-
     expect(res.status).toBe(200);
+  });
+  test("[POST] /regiester api is giving a 400 status if user already exists", async () => {
+    const user = {
+      username: "amyconley",
+      password: "lkavgs",
+      phone_number: 8014344556,
+    };
+    const res = await request("http://localhost:9000/api/users/auth")
+      .post("/register")
+      .send(user);
+    expect(res.status).toBe(400);
   });
 });
