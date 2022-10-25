5a6a65d78678b5b880fd334e2cfcf9bb3a2d020c
David Conley <dconley1989@gmail.com>
2022-10-25 08:43:49 -0600

    added another test and added the rendering method to the first test because I forgot that the first time

--- client/src/Components/_Test_/Dashboard.test.js
+++ client/src/Components/_Test_/Dashboard.test.js
@@ -1,10 +1,25 @@
 import Dashboard from "../Dashboard";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Dashboard module is displaying correctly", () => {
   it("the title is displaying", () => {
+    render(
+      <Router>
+        <Dashboard />
+      </Router>
+    );
     const title = screen.getByText("My Plants");
     expect(title).toBeInTheDocument();
+  });
+  it("they button is displaying correctly", () => {
+    render(
+      <Router>
+        <Dashboard />
+      </Router>
+    );
+    const button = screen.getByRole("button", { name: "Add Plant Card" });
+    expect(button).toBeInTheDocument();
   });
 });
