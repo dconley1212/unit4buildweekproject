61153e0c1d1f4c71f5a2f1bae11037247afca81f
David Conley <dconley1989@gmail.com>
2022-10-27 12:09:37 -0600

    added two more tests to check the 2nd and third labels in the form are showing up in the addPlant module

--- client/src/Components/_Test_/AddPlant.test.js
+++ client/src/Components/_Test_/AddPlant.test.js
@@ -4,13 +4,31 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 describe("AddPlant module is displaying the wrong features", () => {
-  it("first input is displaying correctly on form", () => {
+  it("first input label is displaying correctly on form", () => {
     render(
       <Router>
         <AddPlant />
       </Router>
     );
-    const nickname = screen.getByLabelText("Nickname");
+    const nickname = screen.getByLabelText("Nickname:");
     expect(nickname).toBeInTheDocument();
+  });
+  it("second input label is displaying correctly on form", () => {
+    render(
+      <Router>
+        <AddPlant />
+      </Router>
+    );
+    const species = screen.getByLabelText("Species:");
+    expect(species).toBeInTheDocument();
+  });
+  it("third input label is displaying correctly", () => {
+    render(
+      <Router>
+        <AddPlant />
+      </Router>
+    );
+    const h20Frequency = screen.getByLabelText("h20 Frequency:");
+    expect(h20Frequency).toBeInTheDocument();
   });
 });
