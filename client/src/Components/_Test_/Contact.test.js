a26cde64f757659c865d4127cabf889c5f20f5d9
David Conley <dconley1989@gmail.com>
2022-10-28 07:39:44 -0600

    added two more tests to make sure a couple of paragraphs were showing up in the contact component

--- client/src/Components/_Test_/Contact.test.js
+++ client/src/Components/_Test_/Contact.test.js
@@ -13,4 +13,24 @@ describe("elements are displaying correctly in Contact Component", () => {
     const title = screen.getByText("Questions? Something went wrong?");
     expect(title).toBeInTheDocument();
   });
+  it("paragraph explaining the lead developer is displaying", () => {
+    render(
+      <Router>
+        <Contact />
+      </Router>
+    );
+    const firstParagraph = screen.getByText(
+      "Contact Dave Conley, who is the Lead Developer."
+    );
+    expect(firstParagraph).toBeInTheDocument();
+  });
+  it("paragraph displaying email is in the document", () => {
+    render(
+      <Router>
+        <Contact />
+      </Router>
+    );
+    const emailParagraph = screen.getByText("Email: dconley1989@gmail.com");
+    expect(emailParagraph).toBeInTheDocument();
+  });
 });
